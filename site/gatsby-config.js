233687e7792af4c6d67b6aa26bfd7822bf384a5d
Bettina Swynnerton <bettina.swynnerton@datastax.com>
2020-10-16 10:32:43 +0100

    latest changes

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/ring.ico`,
+        icon: `src/images/ring.png`,
       },
     },
     `gatsby-plugin-postcss`,
